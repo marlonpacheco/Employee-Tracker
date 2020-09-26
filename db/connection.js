e84564dbb2e607ad5ea30f595ed1433596c18a3b
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-09-26 18:14:23 -0500

    setup initial files

---
+++ db/connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql2");
+var util = require("util");
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employees"
+});
+
+connection.connect();
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
